c972173912eb6bc09094c7b2c3281ede6687149b
Phillip Lehner <pl@philliplehner.com>
2021-03-06 20:02:39 +0000

    Updates board image

--- src/game.js
+++ src/game.js
@@ -1,30 +1,11 @@
 import Phaser from 'phaser'
 
 function preload() {
-  this.load.setBaseURL('http://labs.phaser.io')
-  this.load.image('sky', 'assets/skies/space3.png')
-  this.load.image('logo', 'assets/sprites/phaser3-logo.png')
-  this.load.image('red', 'assets/particles/red.png')
+  this.load.image('board', 'assets/medium_board.png')
 }
 
 function create() {
-  this.add.image(400, 300, 'sky')
-
-  const particles = this.add.particles('red')
-
-  const emitter = particles.createEmitter({
-    speed: 100,
-    scale: { start: 1, end: 0 },
-    blendMode: 'ADD',
-  })
-
-  const logo = this.physics.add.image(400, 100, 'logo')
-
-  logo.setVelocity(100, 200)
-  logo.setBounce(1, 1)
-  logo.setCollideWorldBounds(true)
-
-  emitter.startFollow(logo)
+  this.add.image(400, 300, 'board')
 }
 
 const config = {
